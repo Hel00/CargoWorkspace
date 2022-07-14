f19371d04bb4e2d6b6d91e2bd96c75106eb9193b
Hel <11891920+Hel00@users.noreply.github.com>
2022-07-14 17:07:19 +0200

    Add files via upload

--- piano_tile_clicker/src/main.rs
+++ piano_tile_clicker/src/main.rs
@@ -1,31 +1,89 @@
 // https://www.crazygames.com/game/piano-tiles-2-online
 
+use winapi::shared::windef::HDC__;
 use winapi::um::wingdi::*;
 use winapi::um::winuser::*;
 use std::ptr::null_mut;
+use std::{thread, time};
 
 mod colour;
 use enigo::*;
 
+fn clear_console()
+{
+    print!("{esc}c", esc = 27 as char);
+}
+
+/*
+COORDS
+
+X   Y
+620 470
+
+715 470
+
+800 470
+
+900 470
+*/
+
+unsafe fn detect_tile(hdc: *mut HDC__, x: i32, y: i32) -> bool
+{
+    if GetPixel(hdc, x, y) == 0 { true  }
+    else                        { false }
+}
+
+unsafe fn click_tile(hdc: *mut HDC__, enigo: &mut Enigo, x: i32, y: i32)
+{
+    if detect_tile(hdc, x, y) == true
+    {
+        enigo.mouse_move_to(x, y);
+        enigo.mouse_down(MouseButton::Left);
+        enigo.mouse_up(MouseButton::Left);
+    }
+}
+
 fn main()
 {
-    let mut enigo = Enigo::new();
-    enigo.mouse_move_to(500, 200);
-
-    let cursor_location: (i32, i32) = Enigo::mouse_location();
-    
     unsafe
     {
-        let colour_u32 = GetPixel(GetDC(null_mut()), 500, 500);
-        let colour = colour::Colour::new(colour_u32);
+        let SLEEP_TIME = time::Duration::from_secs_f32(0.01);
+        let hdc = GetDC(null_mut());
+        let mut enigo: enigo::Enigo = Enigo::new();
 
-        println!("Raw colour hex: {:#06X}", colour_u32);
-        println!("");
+        let mut cursor_location: (i32, i32);
+        let mut colour_u32: u32;
+        let mut colour: colour::Colour;
 
-        println!("R    G    B    A");
-        println!("{:#04X} {:#04X} {:#04X} {:#04X}", colour.r, colour.g, colour.b, colour.a);
-        println!("");
+        loop
+        {
+            cursor_location = Enigo::mouse_location();
 
-        println!("X: {} Y: {}", cursor_location.0, cursor_location.1);
+            colour_u32 = GetPixel(hdc,
+                cursor_location.0,
+                cursor_location.1);
+
+            colour = colour::Colour::new (colour_u32);
+
+            // DEBUG INFO
+
+            /*clear_console();
+
+            println!("Raw colour hex: {:#06X}", colour_u32);
+            println!("");
+    
+            println!("R    G    B    A");
+            println!("{:#04X} {:#04X} {:#04X} {:#04X}", colour.r, colour.g, colour.b, colour.a);
+            println!("");
+
+            println!("X: {} Y: {}", cursor_location.0, cursor_location.1);*/
+
+            click_tile(hdc, &mut enigo, 620, 570);
+            click_tile(hdc, &mut enigo, 715, 570);
+            click_tile(hdc, &mut enigo, 800, 570);
+            click_tile(hdc, &mut enigo, 900, 570);
+
+            //thread::sleep(SLEEP_TIME);
+        }
     }
 }
