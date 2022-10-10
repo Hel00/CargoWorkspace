77d73713798a0d19770e8a26b9fc781d42f8e14b
Hel <11891920+Hel00@users.noreply.github.com>
2022-10-11 01:16:39 +0200

    Add files via upload

--- bf4_recoil_reduce/src/main.rs
+++ bf4_recoil_reduce/src/main.rs
@@ -1,29 +1,44 @@
 use windows_sys::{ core::*, Win32::{UI::WindowsAndMessaging::*, System::{Threading::{PROCESS_ALL_ACCESS, OpenProcess}, Diagnostics::Debug::ReadProcessMemory}} };
-use std::{ ptr::{self, null}, ffi::c_void, ffi::CString };
+use std::{ ptr::{ null }, ffi::c_void, ffi::CString };
+
+unsafe fn read_memory( handle: isize, offsets: Vec< u64 > ) -> u64
+{
+    let mut buffer = 0_u64;
+    const BUFFER_SIZE: usize = 4;
+
+    for item in offsets
+    {
+        ReadProcessMemory
+        (
+            handle,
+            ( buffer + item ) as *const c_void,
+            &mut buffer as *mut _ as *mut c_void,
+            BUFFER_SIZE,
+            0 as *mut usize
+        );
+    }
+
+    buffer
+}
 
 fn main()
 {
-    let game_name = CString::new("Ring0 Atomic 7.4").unwrap();
+    let process_name = CString::new("Battlefield 4").unwrap();
 
     unsafe
     {
         let mut pid: u32 = 0;
-        let mut status: [u8; 4] = [0, 0, 0, 0];
 
-        let hwnd = FindWindowA(null(), game_name.as_ptr() as *const u8);
+        let hwnd = FindWindowA(null(), process_name.as_ptr() as *const u8);
 
-        let wtpid = GetWindowThreadProcessId(hwnd, &mut pid as *mut u32);
+        GetWindowThreadProcessId(hwnd, &mut pid as *mut u32);
 
         let handle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid as u32);
 
-        ReadProcessMemory(
-            handle,
-            0x07C7C388 as *const c_void, // should read 3_i32
-            &mut status as *mut _ as *mut c_void,
-            status.len(),
-            0 as *mut usize
-        );
+        let result = read_memory(handle, vec![0x1423B2EC8, 0x128, 0x10, 0xE0]);
 
-        println!("status is {:?}", status);
+        println!("result is {}", result);
     }
 }
+
+/* read_memory(handle, vec![0x1423B2EC8, 0x128, 0x10, 0xE0]) = 3_i32 */
