b5bd5df62703a5689250f24c0141faca0a3c77b1
Hel <11891920+Hel00@users.noreply.github.com>
2022-10-11 02:02:41 +0200

    Add files via upload

--- bf4_recoil_reduce/src/main.rs
+++ bf4_recoil_reduce/src/main.rs
@@ -1,5 +1,7 @@
+/* MUST RUN AS ADMIN */
+
 use windows_sys::{ core::*, Win32::{UI::WindowsAndMessaging::*, System::{Threading::{PROCESS_ALL_ACCESS, OpenProcess}, Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory}}} };
-use std::{ ptr::{ null }, ffi::c_void, ffi::CString };
+use std::{ ptr::{ null }, ffi::c_void, ffi::CString, time::Duration };
 
 unsafe fn read_memory( handle: isize, offsets: Vec< u64 > ) -> u64
 {
@@ -38,56 +40,32 @@ unsafe fn write_memory( handle: isize, address: u64, value: [u8; 4] )
 fn main()
 {
     let process_name = CString::new("Battlefield 4").unwrap();
+    const DELAY: Duration = Duration::new(5, 0);
+    let mut pid: u32 = 0;
 
     unsafe
     {
-        let mut pid: u32 = 0;
-
         let hwnd = FindWindowA(null(), process_name.as_ptr() as *const u8);
 
         GetWindowThreadProcessId(hwnd, &mut pid as *mut u32);
 
         let handle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid as u32);
 
-        let result = read_memory(handle, vec![0x1423B2EC8, 0x128, 0x10]) + 0xE0;
+        let sight_recoil = read_memory(handle, vec![0x1423B2EC8, 0x128, 0x30, ]) + 0x430;
+        let sight_spread = read_memory(handle, vec![0x1423B2EC8, 0x128, 0x30, ]) + 0x434;
 
-        write_memory(handle, result, [0, 0, 0, 0]);
+        let hip_recoil = read_memory(handle, vec![0x1423B2EC8, 0x128, 0x30, ]) + 0x438;
+        let hip_spread = read_memory(handle, vec![0x1423B2EC8, 0x128, 0x30, ]) + 0x43C;
+        
+        loop
+        {
+            write_memory(handle, sight_recoil, [0x00, 0x00, 0x00, 0x3F]);
+            write_memory(handle, sight_spread, [0x00, 0x00, 0x00, 0x3F]);
 
-        println!("result is {:#02X?}", result);
+            write_memory(handle, hip_recoil, [0x00, 0x00, 0x00, 0x3F]);
+            write_memory(handle, hip_spread, [0x00, 0x00, 0x00, 0x3F]);
+
+            std::thread::sleep( DELAY );
+        }
     }
 }
-
-/* read_memory(handle, vec![0x1423B2EC8, 0x128, 0x10, 0xE0]) = 3_i32 */
-
-/*
-import winim
-
-proc getOffset(processHandle: HANDLE, offsets: seq[int64]): int64 =
-  var
-    value: int64
-  for i in 0 .. offsets.len - 1:
-    discard ReadProcessMemory(processHandle, cast[LPVOID](value + offsets[i]), LPVOID(addr(value)), SIZE_T(sizeof(value)), nil)
-  return value
-
-type
-  InvadeProc = proc(base: clonglong, signType: cint) {.fastcall.}
-  AddSoulProc = proc(base: clonglong, effect: cint) {.fastcall.}
-
-let
-  processHandle: HANDLE = GetCurrentProcess()
-  BaseB: int64 = 0x144768E78
-  addSoulBase: int64 = getOffset(processHandle, @[BaseB, 0x80, 0x1FA0])
-  invade = cast[InvadeProc](REDACTED)
-  addSoul = cast[AddSoulProc](0x1405A3310)
-
-addSoul(addSoulBase, 42069)
-*/
-
-/*
-println!("{:#02x}", 10);
-println!("{:#0x}", 10);
-println!("{:#x}", 10);
-println!("{:#x?}", 10);
-println!("{:#0x?}", 10);
-println!("{:#02x?}", 10);
-*/
