953f4c95ff680509188ece11a0a80f8ae762f083
Hel <11891920+Hel00@users.noreply.github.com>
2023-03-31 19:35:57 +0200

    Update lib.rs

--- macro_test/macros/src/lib.rs
+++ macro_test/macros/src/lib.rs
@@ -1,5 +1,4 @@
 use std::str::FromStr;
-
 use proc_macro::*;
 
 #[proc_macro]
@@ -11,6 +10,27 @@ pub fn test(input: TokenStream) -> TokenStream
 
     let result = TokenStream::from(x);
     // result = Ok(TokenStream::from(x));
+
+    result
+}
+
+#[proc_macro]
+pub fn print_node(input: TokenStream) -> TokenStream
+{
+    for token in input.clone()
+    {
+        let node = match token.clone()
+        {
+            TokenTree::Group(_) => "Group",
+            TokenTree::Ident(_) => "Ident",
+            TokenTree::Literal(_) => "Literal",
+            TokenTree::Punct(_) => "Punct",
+        };
+
+        println!("Token {} is of Node {}", token, node);
+    }
+
+    let result = TokenStream::from_str("0").unwrap();
 
     result
 }
