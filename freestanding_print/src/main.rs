07d52f562fd6f112ef3b8d87e5aff64ca834484c
Hel <11891920+Hel00@users.noreply.github.com>
2023-04-02 01:38:29 +0200

    Add files via upload

---
+++ freestanding_print/src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+#[no_mangle]
+pub unsafe extern "C" fn write(file_descriptor: i32, stream: &str, size: isize)
+{
+    const SYSCALL_NUMBER_WRITE: i32 = 1;
+    let mut result = 0;
+
+    asm!
+    {
+        "syscall",
+        in("rax") 1,
+        in("rdi") file_descriptor,
+        in("rsi") stream.as_ptr(),
+        in("rdx") size,
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn main()
+{
+    unsafe { write(0, "Hey\n", 4); }
+}
+
+#[panic_handler]
+fn my_panic(_info: &core::panic::PanicInfo) -> !
+{
+    loop {}
+}
